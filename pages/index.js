46dda85279f79e5c7d18cb0e8ddefd6fc3b56b55
Y0UNES <90275505+Y0UNES@users.noreply.github.com>
2024-04-20 14:58:20 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -65,3 +65,16 @@ export function getStaticProps() {
 
   return { props: { posts, globalData } };
 }
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6623bb81a0c6737bd12e8370/1hrtp8t4q';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
